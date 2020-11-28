b797724f30190f09d7a2be8732908f5cb9a350c4
VSarabudla <woodsofnarnia@gail.com>
2020-11-28 17:01:38 -0500

    Add Alien class to represent an enemy

---
+++ include/core/alien.h
@@ -0,0 +1,57 @@
+#ifndef FINAL_PROJECT_ALIEN_H
+#define FINAL_PROJECT_ALIEN_H
+
+#include "cinder/gl/gl.h"
+#include "core/bullet.h"
+
+namespace shooter {
+
+class Alien {
+ public:
+  /**
+   * Creates an Alien object
+   *
+   * @param position - position of alien
+   * @param movement_speed - movement speed of alien
+   */
+  Alien(const glm::vec2 &position, float movement_speed);
+
+  /**
+   * Move alien upwards based on movement speed
+   */
+  void MoveUp();
+
+  /**
+   * Move alien downwards based on movement speed
+   */
+  void MoveDown();
+
+  /**
+   * Move alien left based on movement speed
+   */
+  void MoveLeft();
+
+  /**
+   * Move alien right based on movement speed
+   */
+  void MoveRight();
+
+  /**
+   * Returns a Bullet object traveling in the direction of the player in the
+   * application window
+   *
+   * @param player_position - position of the player in the application window
+   * @return
+   */
+  Bullet ShootBullet(const glm::vec2 &player_position);
+
+  glm::vec2 GetPosition() const &;
+
+ private:
+  glm::vec2 position_;
+  float movement_speed_;
+};
+
+}  // namespace shooter
+
+#endif  // FINAL_PROJECT_ALIEN_H
---
+++ src/core/alien.cc
@@ -0,0 +1,31 @@
+#include "core/alien.h"
+
+shooter::Alien::Alien(const glm::vec2 &position, float movement_speed)
+    : position_(position), movement_speed_(movement_speed) {
+}
+
+shooter::Bullet shooter::Alien::ShootBullet(const glm::vec2 &player_position) {
+  return shooter::Bullet(
+      position_, movement_speed_ * glm::normalize(player_position - position_),
+      movement_speed_ / 2, ci::Color("green"));
+}
+
+glm::vec2 shooter::Alien::GetPosition() const & {
+  return position_;
+}
+
+void shooter::Alien::MoveUp() {
+  position_ += glm::vec2(0, -movement_speed_);
+}
+
+void shooter::Alien::MoveDown() {
+  position_ += glm::vec2(0, movement_speed_);
+}
+
+void shooter::Alien::MoveLeft() {
+  position_ += glm::vec2(-movement_speed_, 0);
+}
+
+void shooter::Alien::MoveRight() {
+  position_ += glm::vec2(movement_speed_, 0);
+}
